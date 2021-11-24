e7715cf276314a8db8695fcc5b20305a8a531a7f
guohaoxian <guohx2010@163.com>
2021-11-25 00:10:02 +0800

    修改head css

--- src/pages/Frontend/Home.tsx
+++ src/pages/Frontend/Home.tsx
@@ -1,71 +1,22 @@
-import { css, cx } from '@emotion/css';
-import { useHead } from '@vueuse/head';
-import styled from '@emotion/styled'
+// import { css, cx, sheet, cache, getRegisteredStyles } from "@emotion/css";
+import { useHead } from "@vueuse/head";
+import { compile, serialize, stringify, middleware, prefixer } from "stylis";
 
+function css(style: TemplateStringsArray | string) {
+  let _style = typeof style === "string" ? style : style.join("");
+  return serialize(compile(_style), middleware([prefixer, stringify]));
+}
 
-let div = styled.div`
+const headStyle = css`
+  background-color: red;
+  color: blue;
   color: red;
-  .inner{
-    background-color: blue;
-    .child{
-      text-align: unset;
-    }
-  }
-`
-
-console.log(div)
-
-const mainCls = css`
-  min-height: 100vh;
-  overflow: hidden;
-  background-image: linear-gradient(to top, #fddb92 0%, #d1fdff 100%);
-  h1 {
-    display: none;
-  }
-  .nav {
-    width: 80%;
-    margin: 100px auto;
-    display: flex;
-    .item {
-      margin: 1%;
-      width: 23%;
-      height: 0;
-      padding-bottom: 23%;
-      position: relative;
-      .inner {
-        position: absolute;
-        left: 0;
-        right: 0;
-        top: 0;
-        bottom: 0;
-        background-color: #e3d6fb;
-        border-radius: 2vw;
-        display: flex;
-        span {
-          margin: auto;
-        }
-      }
-      // width: 25vw;
-      // height: 25vw;
-      // display: flex;
-      // .inner {
-      //   margin: auto;
-      //   width: 23vw;
-      //   height: 23vw;
-      //   background: lightblue;
-      //   border-radius: 4px;
-      // }
-    }
-  }
-`
-
-console.log(mainCls)
-
-let style = `
-  .home-page{
+  text-align: center;
+  background-color: red;
+  .home-page {
     min-height: 100vh;
     overflow: hidden;
-    background-image: linear-gradient(to top, #fddb92 0%, #d1fdff 100%);
+    // background-image: linear-gradient(to top, #fddb92 0%, #d1fdff 100%);
     h1 {
       display: none;
     }
@@ -105,7 +56,7 @@ let style = `
       }
     }
   }
-`
+`;
 
 export default {
   setup() {
@@ -120,38 +71,40 @@ export default {
       ],
       style: [
         {
-          children: style,
+          children: headStyle,
         },
-      ]
-    })
+      ],
+    });
 
     return () => (
-      <div class="home-page">
-        <h1>Hosea爱生活</h1>
-        <div class="nav">
-          <div class="item">
-            <router-link to="/article" class="inner">
-              <span>文章</span>
-            </router-link>
-          </div>
-          <div class="item">
-            <router-link to="/article" class="inner">
-              <span>文章</span>
-            </router-link>
-          </div>
-          <div class="item">
-            <router-link to="/article" class="inner">
-              <span>文章</span>
-            </router-link>
-          </div>
-          <div class="item">
-            <router-link to="/article" class="inner">
-              <span>文章</span>
-            </router-link>
+      <div>
+        <div class="home-page">
+          hello world
+          {/* <h1>Hosea爱生活</h1> */}
+          <div class="nav">
+            <div class="item">
+              <router-link to="/article" class="inner">
+                <span>文章</span>
+              </router-link>
+            </div>
+            <div class="item">
+              <router-link to="/article" class="inner">
+                <span>文章</span>
+              </router-link>
+            </div>
+            <div class="item">
+              <router-link to="/article" class="inner">
+                <span>文章</span>
+              </router-link>
+            </div>
+            <div class="item">
+              <router-link to="/article" class="inner">
+                <span>文章</span>
+              </router-link>
+            </div>
           </div>
         </div>
       </div>
-    )
-  }
-}
-
+    );
+  },
+};
